755ab975fc54c2ccd41670e32d3302bffc457dbd
saikhmirsat <saikh.mirsat.786@gmail.com>
2023-02-28 17:17:06 +0530

    add btn

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -9,12 +9,14 @@ import { MdLightMode, MdOutlineLightMode } from 'react-icons/md';
 import { ThemeContext } from '../Context/ThemeContext';
 import { useContext } from 'react';
 // import { motion } from 'framer-motion'
+import Resume from '../Images/fw19_0481-Saikh-Mirsat.pdf'
 
 export default function Navbar({ homeHandle, aboutHandle, skillsHandle, projectHandle, contactHandle }) {
     const [isMobile, setIsMobile] = useState(false)
     const [navbar, setNavbar] = useState(false)
 
     const { themeHandle, theme } = useContext(ThemeContext)
+    console.log(theme)
 
     const changeBackground = () => {
         if (window.scrollY >= 80) {
@@ -25,6 +27,13 @@ export default function Navbar({ homeHandle, aboutHandle, skillsHandle, projectH
     }
     window.addEventListener('scroll', changeBackground)
 
+    // const handelClick = () => {
+    //     window.open("https://drive.google.com/file/d/1HxjdMB0DAuU5IfDJJvbWpfoq5d_0oTfp/view?usp=sharing", "_blank")
+    // }
+
+    const handelDownload = () => {
+        window.open("https://drive.google.com/file/d/1HxjdMB0DAuU5IfDJJvbWpfoq5d_0oTfp/view?usp=sharing", "_blank")
+    }
 
     return (
         <div className={navbar ? "navbar active" : "navbar"} >
@@ -39,9 +48,20 @@ export default function Navbar({ homeHandle, aboutHandle, skillsHandle, projectH
                 <li
                     onClick={skillsHandle}><h4>Skills</h4></li>
                 <li
-                    onClick={projectHandle}><h4>Projects</h4></li>
-                {/* <li onClick={resumeHandle}><h4>Resume</h4></li> */}
-                <li onClick={contactHandle}><h4 >Contact & Resume</h4></li>
+                    onClick={projectHandle}><h4>Projects</h4>
+                </li>
+                <li onClick={contactHandle}><h4 >Contact</h4></li>
+                <li className='nav-resume-btn-con'><a
+                    id={theme && "dark"}
+                    href={Resume}
+                    target="_blank"
+                    download={"fw19_0481-Saikh-Mirsat"}
+                    _hover={{ TextDecoder: "none" }}
+                ><h4
+                    download={Resume}
+                    onClick={() => handelDownload()}
+                >Resume</h4></a></li>
+
 
                 <li className='themeBtn' onClick={themeHandle}>{theme ? <MdLightMode style={{ color: 'black' }} /> : <MdOutlineLightMode style={{ color: 'yellow' }} />}</li>
             </ul>
